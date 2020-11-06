55c9ba262d6a31435dbe850d38c3935de19cef6b
KowalczykEmil <pomocyakoshi@gmail.com>
2020-11-06 20:58:11 +0100

    Created new class and implemented methods from interface

---
+++ usbdevice/MemoryStick.java
@@ -0,0 +1,38 @@
+package com.emilkowalczyk.usbdevice;
+
+public class MemoryStick implements USBDevice{
+    private String name;
+    private boolean ejected = false;
+
+
+    public MemoryStick(String name){
+        this.name = name;
+    }
+
+    @Override
+    public boolean connect() {
+        System.out.println("MemoryStick connected");
+        return true;
+    }
+
+    @Override
+    public boolean disconnect() {
+        if(!ejected) {
+            System.out.println("Proszę wypnij najpierw kartę pamięci");
+            return false;
+        } else {
+            System.out.println("MemoryStick disconnected");
+            return true;
+        }
+    }
+
+    public void eject(){
+        System.out.println("Ejecting Memory Stick");
+        ejected = true;
+    }
+    
+    @Override
+    public String getName() {
+        return name;
+    }
+}
