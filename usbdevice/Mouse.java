6ddba71602ca850a923bfe1d6b3792c432fec95d
KowalczykEmil <pomocyakoshi@gmail.com>
2020-11-06 20:48:35 +0100

    Implemented USBDevice-interface methods to class Mouse.java

--- usbdevice/Mouse.java
+++ usbdevice/Mouse.java
@@ -1,18 +1,26 @@
 package com.emilkowalczyk.usbdevice;
 
 public class Mouse implements USBDevice {
+    private String name;
+
+    public Mouse(String name){
+        this.name = name;
+    }
+
     @Override
     public boolean connect() {
-        return false;
+        System.out.println("Mouse connected");
+        return true;
     }
 
     @Override
     public boolean disconnect() {
-        return false;
+        System.out.println("Mouse disconnected");
+        return true;
     }
 
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 }
